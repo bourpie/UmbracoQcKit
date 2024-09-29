0ab46271ba53dbe8c9785a2e4837e61fb7cf7a52
Pierre Bourgeois <pbourgeois@soquij.qc.ca>
2024-09-29 06:22:27 -0400

    episode 5.2

---
+++ Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace UmbracoQcKit.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static Home? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<Home>();
+        }
+
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent) 
+        { 
+            var homepage = GetHomePage(publishedContent);
+            if (homepage == null) return null;
+
+            return homepage.FirstChild<SiteSettings>();
+        }
+    }
+}
